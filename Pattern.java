c46d326713be5d62ea3d70833fb2ecc9dc96cd01
sanket232005 <sanketbandgar991@gmail.com>
2025-06-24 22:20:05 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+package com.pattern;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i,j;
+        for(i=0;i<4;i++) {
+        	   for(j=0;j<4;j++) {
+        		System.out.print("   * ");
+        	}
+        	System.out.println();
+        }
+	}
+
+}
